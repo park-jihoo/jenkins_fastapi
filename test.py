094bb7745f9c6f3c06da2625ec30e6047710da60
jihoopark <wlgn8648@gmail.com>
2023-05-16 19:13:59 +0900

    remove Dockerfile

--- test.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-def test_happy_path_name_returns_hello_name(self):
-    app = FastAPI()
-    response = app.get("/John")
-    assert response.status_code == 200
-    assert response.json() == {"Hello": "John"}
-
